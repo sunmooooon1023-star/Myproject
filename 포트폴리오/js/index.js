0b537d35116905b732f2d1eb499578ae0d09cd76
sunmooooon1023 <sunmooooon1023@gmail.com>
2025-09-09 16:03:42 +0900

    1차 완료

--- 포트폴리오/js/index.js
+++ 포트폴리오/js/index.js
@@ -1,5 +1,7 @@
 window.addEventListener('DOMContentLoaded', () => {
-  document.body.classList.add('scroll-block');
+  
+  document.documentElement.classList.add('scroll-block'); // <html>
+  document.body.classList.add('scroll-block');            // <body>
 
   setTimeout(() => {
     document.querySelector('.main_left_text > h2').classList.add('animate');
@@ -16,7 +18,12 @@ window.addEventListener('DOMContentLoaded', () => {
     document.querySelector('.main>.main_right>.main_right_bottom > img').classList.add('animate');
   }, 2600);
 
+  document.querySelectorAll('.scrollText').forEach(el => {
+    el.textContent = el.textContent.repeat(50);
+  });
+
   setTimeout(() => {
+    document.documentElement.classList.remove('scroll-block');
     document.body.classList.remove('scroll-block');
   }, 7000);
 });
